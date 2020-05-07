72ff4289e96831834bd0e1dcadafc7ff5b095e01
ZhengSSQuan <64956071+ZhengSSQuan@users.noreply.github.com>
2020-05-07 22:39:48 +0800

    0.00版 测验代码 c
    草稿

--- Sovereign Code.c
+++ Sovereign Code.c
@@ -1,11 +1,23 @@
-public int[][] num;
-public int[] result;
-public int[] Tresult;
-int right;
-int Length=sizeof(result)/sizeof(result(0);
-for(i=0;i<Length;i++){
-if(result==Tresult){
-right++;}}
-if(right==Length)
-{print("You Pass");}
-else{print("Error,Right=%d,Totle=%d",right,Length);}
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public int[1][2] num;       //1.组号 2.组内数据
+        public int[1] result;       //得到的运算结果
+        public int[1] Tresult;      //正确结果
+        static void Main(string[] args)
+        {
+
+        int right=0;                //正确数
+        int Length = (result.Length-1); //组数
+        for(int i=0;i<Length;i++){
+        if(result==Tresult){
+        right++;}}                  //计算正确数
+        if(right==Length)           //判断是否全对
+        { Console.WriteLine("You Pass");}
+        else{ Console.WriteLine("Error,Right=%d,Totle=%d", right, Length);}
+        }                           //错误显示正确数，总组数
+    }
+}
