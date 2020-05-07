e19b7436616207f73c46a866fca7c8281c7ba220
ZhengSSQuan <64956071+ZhengSSQuan@users.noreply.github.com>
2020-05-07 22:10:29 +0800

    Create Sovereign Code.c
    总码

---
+++ Sovereign Code.c
@@ -0,0 +1,11 @@
+public int[][] num;
+public int[] result;
+public int[] Tresult;
+int right;
+int Length=sizeof(result)/sizeof(result(0);
+for(i=0;i<Length;i++){
+if(result==Tresult){
+right++;}}
+if(right==Length)
+{print("You Pass");}
+else{print("Error,Right=%d,Totle=%d",right,Length);}
